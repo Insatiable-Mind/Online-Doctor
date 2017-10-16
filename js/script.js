00672849b2d4d8dfd7f2c4459ee967286431bd9c
Insatiable_Mind <o.a.chernyakova@gmail.com>
2017-10-16 23:22:20 +0300

    make code more readable and substitute var for let

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 /****** СТРОКА ПОИСКА ВРАЧЕЙ ******/
-var doctorsNav = document.querySelector('.doctors-nav'),
-    searchContainer = document.querySelector('.doctors-nav__search'),
-    search = document.querySelector('.doctors-nav__search-input'),
-    searchClearButton = document.querySelector('.doctors-nav__search-clear');
+let search = document.querySelector('.doctors-nav__search-input');
+let searchClearButton = document.querySelector('.doctors-nav__search-clear');
 
 search.addEventListener('keypress', function() {
   searchClearButton.style.display = 'block';
@@ -16,31 +14,34 @@ search.addEventListener('focus', function() {
   }
 });
 
+let doctorsNav = document.querySelector('.doctors-nav');
 doctorsNav.addEventListener('click', closeSearch);
 
-/*
-  closeSearch() уменьшает ширину input-а ("закрывает его"),
-  когда пользователь кликает вне этого элемента
-*/
+/**
+ *closeSearch() уменьшает ширину input-а ("закрывает его"),
+ *когда пользователь кликает вне этого элемента
+ */
 function closeSearch(e) {
-  if ( isMouseOver(searchContainer, e) === false ) {
+  let searchContainer = document.querySelector('.doctors-nav__search');
+
+  if (isMouseOver(searchContainer, e) === false) {
     search.classList.remove('doctors-nav__search-input_open');
     searchClearButton.style.display = 'none';
-
     search.removeEventListener('transitionend', showClearButton);
   }
 }
 
 function isMouseOver(elem, e) {
-  var elemCoordinates = elem.getBoundingClientRect();
+  let elemCoordinates = elem.getBoundingClientRect();
 
-  if ( (e.clientX >= elemCoordinates.left) && (e.clientX <= elemCoordinates.right) ) {
-    if ( (e.clientY >= elemCoordinates.top) && (e.clientY <= elemCoordinates.bottom) ) {
-      return true;
-    } 
-
-    return false;
-  }
+  if (
+    (e.clientX >= elemCoordinates.left) &&
+    (e.clientX <= elemCoordinates.right) &&
+    (e.clientY >= elemCoordinates.top) &&
+    (e.clientY <= elemCoordinates.bottom)
+  ) {
+    return true;
+  } 
 
   return false;
 }
@@ -51,7 +52,7 @@ function showClearButton() {
 
 
 /****** КНОПКИ С ВЫПАДАЮЩИМ СПИСКОМ ДЛЯ ФИЛЬТРАЦИИ ВРАЧЕЙ ******/
-var dropdown = document.querySelectorAll('.doctors-nav__dropdown');
+let dropdown = document.querySelectorAll('.doctors-nav__dropdown');
 
 dropdown[0].addEventListener('click', function() {
   openDropDownMenu(this);
@@ -71,8 +72,8 @@ function openDropDownMenu(dropdown) {
 }
 
 function changeBorderRadius(dropdown) {
-  var dropdownList = dropdown.querySelector('.doctors-nav__dropdown-list'),
-      dropdownBtn = dropdown.querySelector('.doctors-nav__dropdown-btn');
+  let dropdownList = dropdown.querySelector('.doctors-nav__dropdown-list');
+  let dropdownBtn = dropdown.querySelector('.doctors-nav__dropdown-btn');
 
   if (dropdownList.offsetWidth === dropdownBtn.offsetWidth) {
     dropdownList.style.borderTopRightRadius = '0';
@@ -83,11 +84,12 @@ function changeBorderRadius(dropdown) {
 
 
 /****** ВКЛАДКИ НАД РЕЗУЛЬТАТАМИ ПОИСКА ПО ВРАЧАМ ******/
-var doctorsTabs = document.querySelectorAll('.search-result__title'),
-    backgroundElem = document.createElement('div');
+let backgroundElem = document.createElement('div');
 
 backgroundElem.classList.add('search-result__title_background');
 document.querySelector('.search-result__header').appendChild(backgroundElem);
+
+let doctorsTabs = document.querySelectorAll('.search-result__title');
 
 doctorsTabs[0].addEventListener('click', function(e) {
   e.preventDefault();
@@ -103,13 +105,18 @@ doctorsTabs[1].addEventListener('click', function(e) {
 
 function makeTabActive(elem) {
   elem.classList.add('search-result__title_active');
-  document.querySelector('.search-result__title_background').style.left = (elem.getBoundingClientRect().left - document.querySelector('.search-result__header').getBoundingClientRect().left) + 'px';
-  document.querySelector('.search-result__title_background').style.width = elem.offsetWidth + 'px';
+
+  let header = document.querySelector('.search-result__header');
+  let headerLeft = header.getBoundingClientRect().left;
+  let elemLeft = elem.getBoundingClientRect().left;
+
+  backgroundElem.style.left = elemLeft - headerLeft + 'px';
+  backgroundElem.style.width = elem.offsetWidth + 'px';
 }
 
 
 /****** КНОПКИ "ДОБАВИТЬ В ИЗБРАННЫЕ" ******/
-var favouritesBtn = document.querySelectorAll('.specialist__favourites');
+let favouritesBtn = document.querySelectorAll('.specialist__favourites');
 
 favouritesBtn[0].addEventListener('click', function() {
   addToFavourites(this);
@@ -120,13 +127,11 @@ favouritesBtn[1].addEventListener('click', function() {
 });
 
 function addToFavourites(node) {
-  var favouritesHeart = node.querySelector('.heart-icon'),
-      favouritesText = node.querySelector('.specialist__favourites-text');
+  let favouritesHeart = node.querySelector('.heart-icon');
+  let favouritesText = node.querySelector('.specialist__favourites-text');
 
   node.classList.toggle('specialist__favourites_active');
-
   changeText(node, favouritesText);
-
   animate(favouritesText, 'fadeIn');
   animate(favouritesHeart, 'increase');
 }
